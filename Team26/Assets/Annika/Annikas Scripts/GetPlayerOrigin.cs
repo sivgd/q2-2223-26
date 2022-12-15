e5c5963a36e53948ec0460742f2b6a399cd7f551
MUKILTEO\1630357 <1630357@mukilteo.wednet.edu>
2022-12-15 12:46:33 -0800

    Organized scripts, added more to player, etc.

---
+++ Team26/Assets/Annika/Annikas Scripts/GetPlayerOrigin.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPlayerOrigin : MonoBehaviour
+{
+
+    private Vector3 playerOrigin;
+    public GameObject mainPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerOrigin = mainPlayer.transform.position;
+        transform.localPosition = playerOrigin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
