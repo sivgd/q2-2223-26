fb56d2768ad4e13f3da7b1ac4374f22990e6fcf8
MUKILTEO\1713882 <1713882@mukilteo.wednet.edu>
2022-12-16 14:32:12 -0800

    Added to game level

--- Team26/Assets/Dylan/TraversePlatform.cs
+++ Team26/Assets/Dylan/TraversePlatform.cs
@@ -3,16 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TraversePlatform : MonoBehaviour
-{
-    // Start is called before the first frame update
+{// Start is called before the first frame update
+
+    public List<Transform> patrolPts = new List<Transform>();
+    Transform[] patrolPoints = new Transform[] { };
+    public int patrolPath;
+    public float pathTime;
+    public float currentTime;
+    int nPoints;
+    public GameObject player;
+
     void Start()
     {
-        
+        patrolPath = Random.Range(0, patrolPts.Count);
+        Debug.Log(patrolPath);
+        patrolPoints = patrolPts.ToArray();
+        nPoints = patrolPoints.Length;
+
+
+        //  turnTowardsTarget();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime += Time.deltaTime;
+        if (currentTime > pathTime)
+        {
+
+
+            currentTime = 0; //reset the timer
+            patrolPath++;
+            if (patrolPath > nPoints - 1) patrolPath = 0;
+
+
+
+            //  turnTowardsTarget();
+
+        }
+        transform.position = Vector3.Lerp(patrolPoints[patrolPath].position, patrolPoints[(patrolPath + 1) % nPoints].position, currentTime / pathTime);
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
     }
 }
