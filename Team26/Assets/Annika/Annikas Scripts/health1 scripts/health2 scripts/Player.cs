a0c568d03e4dea524dacadfd7a8ea60e51e28414
MUKILTEO\1630357 <1630357@mukilteo.wednet.edu>
2023-01-10 13:09:55 -0800

    script edits and moved title scene into demo folder

---
+++ Team26/Assets/Annika/Annikas Scripts/health1 scripts/health2 scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+
+    public Heart heart;
+    //public EnemyDamage damg;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        heart.NumberOfHearts -= damage;
+    }
+}
