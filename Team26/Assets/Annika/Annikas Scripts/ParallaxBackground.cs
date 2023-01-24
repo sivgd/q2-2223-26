bcc33be87fb6be52437e05ca5e076c6bf9cb382c
MUKILTEO\1630357 <1630357@mukilteo.wednet.edu>
2023-01-24 12:47:34 -0800

    parallaxing stuff and enemy

--- Team26/Assets/Annika/Annikas Scripts/ParallaxBackground.cs
+++ Team26/Assets/Annika/Annikas Scripts/ParallaxBackground.cs
@@ -8,11 +8,15 @@ public class ParallaxBackground : MonoBehaviour
 
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
 
     private void Start()
     {
         cameraTransform = Camera.main.transform;
         lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     }
 
     private void LateUpdate()
@@ -21,5 +25,11 @@ public class ParallaxBackground : MonoBehaviour
         //float parallaxEffectMultiplier = .8f;
         transform.position += deltaMovement * parallaxEffectMultiplier;
         lastCameraPosition = cameraTransform.position;
+
+        if(Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }
     }
 }
