e6719f9e5466dcd757a8a4cc604c31332bb2efa6
MUKILTEO\1713882 <1713882@mukilteo.wednet.edu>
2023-01-31 12:52:40 -0800

    Added jump sound

---
+++ Team26/Assets/JumpSound.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpSound : MonoBehaviour
+{
+
+    public AudioSource jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Jumpsound()
+    {
+        jump.Play();
+    }
+}
