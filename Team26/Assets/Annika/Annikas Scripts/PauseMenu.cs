585a8405ef595fc1a66ec02632df98d7e736647d
MUKILTEO\1630357 <1630357@mukilteo.wednet.edu>
2023-02-02 13:31:33 -0800

    pause menu done

--- Team26/Assets/Annika/Annikas Scripts/PauseMenu.cs
+++ Team26/Assets/Annika/Annikas Scripts/PauseMenu.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public static bool isPaused;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,40 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
