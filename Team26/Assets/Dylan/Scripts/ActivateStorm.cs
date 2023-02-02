90ce8c6751b812b2eeaef913bbd5579e8bb649c0
HOTICECUBE\Dylan <1713882@mukilteo.wednet.edu>
2023-02-01 21:37:56 -0800

    Audio Switch Done

--- Team26/Assets/Dylan/Scripts/ActivateStorm.cs
+++ Team26/Assets/Dylan/Scripts/ActivateStorm.cs
@@ -8,13 +8,19 @@ public class ActivateStorm : MonoBehaviour
    
     public Material cloudZone;
     public Material stormZone;
-    
+    public AudioSource _AudioSource;
+
+    public AudioClip _AudioClip1;
+    public AudioClip _AudioClip2;
+
     // Start is called before the first frame update
     void Start()
     {
 
         RenderSettings.skybox = cloudZone;
+        _AudioSource.clip = _AudioClip1;
 
+        _AudioSource.Play();
 
     }
 
@@ -29,19 +35,27 @@ public class ActivateStorm : MonoBehaviour
         StartCoroutine(DelayedAction());
         StartCoroutine(DelayedActionYes());
 
-    }
 
+    }
     private IEnumerator DelayedAction()
     {
         yield return new WaitForSeconds(4);
         RenderSettings.skybox = stormZone;
         rain.GetComponent<ParticleSystem>();
+        _AudioSource.clip = _AudioClip2;
+        _AudioSource.Play();
     }
 
     private IEnumerator DelayedActionYes()
     {
         yield return new WaitForSeconds(34);
         RenderSettings.skybox = cloudZone;
+
+        _AudioSource.clip = _AudioClip1;
+        _AudioSource.Play();
+
+
+
     }
 
 }
