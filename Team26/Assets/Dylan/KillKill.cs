17640f4964585984b58cb78a0aac65f36efa1b49
HOTICECUBE\Dylan <1713882@mukilteo.wednet.edu>
2023-02-10 22:05:18 -0800

    Killboxes do 1 damage

--- Team26/Assets/Dylan/KillKill.cs
+++ Team26/Assets/Dylan/KillKill.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class KillKill : MonoBehaviour
 {
+
+    public GameObject spawnpoint;
+    public GameObject mainPlayer;
+    private Rigidbody2D rb2;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Heart.health = Heart.health - 3;
+            Heart.health = Heart.health - 1;
             if (Heart.health <= 0)
             {
                 //AudioManager.isGameOver = true;
@@ -17,9 +22,16 @@ public class KillKill : MonoBehaviour
             {
                 StartCoroutine(GetHurt());
             }
-            collision.gameObject.GetComponent<Player>().TakeDamage(3);
+            collision.gameObject.GetComponent<Player>().TakeDamage(1);
         }
+
+        rb2 = mainPlayer.GetComponent<Rigidbody2D>();
+        mainPlayer.transform.localPosition = spawnpoint.transform.localPosition;
+
     }
+
+
+
     IEnumerator GetHurt()
     {
         Physics2D.IgnoreLayerCollision(6, 8);
--- Team26/Assets/Dylan/ResetTraverse.cs
+++ Team26/Assets/Dylan/ResetTraverse.cs
@@ -7,41 +7,49 @@ public class ResetTraverse : MonoBehaviour
     public GameObject spawnpoint;
     public GameObject mainPlayer;
     private Rigidbody2D rb2;
-    //public Animator anim;
+    public Animator anim;
     public Material cloudZone;
 
     void Start()
     {
         
-        //anim = gameObject.GetComponent<Animator>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    
-
-    //}
-
-
-   
-
-
-    
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-
-      
-        
-
-
-
        
-            rb2 = mainPlayer.GetComponent<Rigidbody2D>();
+     RenderSettings.skybox = cloudZone;
+        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Heart.health = Heart.health - 1;
+            if (Heart.health <= 0)
+            {
+                //AudioManager.isGameOver = true;
+            }
+            else
+            {
+                StartCoroutine(GetHurt());
+            }
+            collision.gameObject.GetComponent<Player>().TakeDamage(1);
+        }
+        rb2 = mainPlayer.GetComponent<Rigidbody2D>();
             mainPlayer.transform.localPosition = spawnpoint.transform.localPosition;
             Debug.Log("Hello");
-            //anim.SetBool("go", false);
-            RenderSettings.skybox = cloudZone;
-       
-        
+            anim.SetBool("go", false);
+
+
+    }
+
+
+
+
+
+    IEnumerator GetHurt()
+    {
+        Physics2D.IgnoreLayerCollision(6, 8);
+        yield return new WaitForSeconds(2);
+        Physics2D.IgnoreLayerCollision(6, 8, false);
     }
 }
